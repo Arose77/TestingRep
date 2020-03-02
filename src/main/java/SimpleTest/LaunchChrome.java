2146e6e41a1c17e3d559e47d0f9575168b5a9b91
arose77 <ammurose45@gmail.com>
2020-03-02 12:06:58 +0530

    first commit

---
+++ src/main/java/SimpleTest/LaunchChrome.java
@@ -0,0 +1,11 @@
+package SimpleTest;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Simply print");
+
+	}
+
+}
